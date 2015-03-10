ca9ff42e5fc1c934a739db51f8e31dfb9cb296c3
Darin Critchlow <darincritchlow@gmail.com>
2015-03-10 11:53:50 -0600

    add code to populate test mysql database in vagrant box

--- provision.sh
+++ provision.sh
@@ -12,6 +12,8 @@ function say {
   printf "\n--------------------------------------------------------\n"
 }
 
+db='user'
+
 # Install Apache
 say "Installing Apache and setting it up."
   # Update aptitude library
@@ -27,12 +29,30 @@ say "Installing Apache and setting it up."
   # Enable mod_rewrite
   a2enmod rewrite >/dev/null 2>&1
 
+# Install mysql
+say "Installing MySQL."
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update >/dev/null 2>&1
+  apt-get install -y mysql-server >/dev/null 2>&1
+  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/my.cnf
+  service mysql restart >/dev/null 2>&1
+  mysql -u root mysql <<< "GRANT ALL ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;"
+
 say "Installing handy packages"
   apt-get install -y curl git-core ftp unzip imagemagick vim colordiff gettext graphviz nodejs npm >/dev/null 2>&1
 
+say "Creating the database '$db'"
+  mysql -u root -e "create database $db"
+
+#
+# We are populating the msyql database with dump file. In this example it's called user.sql
+#
+say "Populating Database"
+  mysql -u root -D $db < /vagrant/src/$db.sql
+
 say "Installing PHP Modules"
   # Install php 5.6
-  add-apt-repository -y ppa:ondrej/php5-5.6
+  add-apt-repository -y ppa:ondrej/php5-5.6 >/dev/null 2>&1
   apt-get update > /dev/null 2>&1
   # Install php5, libapache2-mod-php5, php5-mysql curl php5-curl
   apt-get install -y php5 php5-cli php5-common php5-dev php5-imagick php5-imap php5-gd libapache2-mod-php5 php5-mysql php5-curl php5-sqlite >/dev/null 2>&1
